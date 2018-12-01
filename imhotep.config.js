e0fddd444a282f7c9dfa1e7349ce85998ad23d32
Gregor Adams <greg@pixelass.com>
2018-12-01 18:24:01 +0100

    chore: added lottie for demo. Add required build options

--- imhotep.config.js
+++ imhotep.config.js
@@ -1,12 +1,24 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const plugins = [new CopyWebpackPlugin([{from: "app/**/*.json", to: "data/", flatten: true}])];
+
 module.exports = {
 	app: {
 		path: "app"
 	},
 	entry: "index.tsx",
+	env: {
+		development: {
+			plugins
+		},
+		production: {
+			plugins
+		}
+	},
 	output: {
 		path: "public"
 	},
 	src: {
 		path: "src"
-	}
+	},
+	ignore: ["**/package.json", "**/data/markus.json"]
 };
