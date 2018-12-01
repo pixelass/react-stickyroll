5aabfa6c66a5adaeae9565a19b841a4606acf2ab
Gregor Adams <greg@pixelass.com>
2018-12-01 01:04:52 +0100

    feat(decorators) Adds decorators for page and progress

---
+++ packages/decorators/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./page";
+export * from "./progress";
+
---
+++ packages/decorators/src/page.tsx
@@ -0,0 +1,25 @@
+import {ScrollConsumer} from "@stickyroll/context";
+import React from "react";
+
+/**
+ * @typedef {function} TPage
+ * @param {React.ComponentClass} BaseComponent
+ * @return {any}
+ */
+export type TPage = (
+	BaseComponent: React.ComponentClass
+) => any;
+
+/**
+ * Add page and pages to the component properties
+ * @type {TPage}
+ */
+export const page: TPage = (BaseComponent) => {
+	return (props: any) => (
+		<ScrollConsumer>
+		{({page, pages}) => (
+			<BaseComponent {...props} page={page} pages={pages}/>
+		)}
+	</ScrollConsumer>
+	);
+};
