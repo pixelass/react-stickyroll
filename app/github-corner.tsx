640a99eb28dd90929545b258cd35951aa1267a6f
Gregor Adams <greg@pixelass.com>
2018-12-02 06:51:44 +0100

    docs: fix link to source

--- app/github-corner.tsx
+++ app/github-corner.tsx
@@ -18,7 +18,7 @@ const StyledCorner: StyledComponent<"svg", any> = styled.svg.attrs({
 `;
 export const GithubCorner = props => (
 	<StyledCorner>
-		<a href="https://github.com/pixelass/react-over-scroll" aria-label="View source on GitHub">
+		<a href="https://github.com/stickyroll/react-stickyroll" aria-label="View source on GitHub">
 			<path fill="#3F51B5" d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
 			<path
 				d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
