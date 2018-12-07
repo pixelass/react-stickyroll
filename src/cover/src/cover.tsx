a8020d49e74d43bd1945753761934e096d4a9066
Gregor Adams <greg@pixelass.com>
2018-12-07 09:29:52 +0100

    docs: fix globalStyles

--- src/cover/src/cover.tsx
+++ src/cover/src/cover.tsx
@@ -1,27 +1,10 @@
 import React from "react";
 import {DeviceSupport} from "./device-spport";
+import {GlobalStyle} from "./styles";
 
-export default class extends React.Component {
-	componentDidMount() {
-		// This is needed due to patternplate not rendering CSS injections correctly.
-		const {body} = document;
-		const style = document.createElement("style");
-		style.innerHTML = `
-			body {
-				margin: 0;
-				font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-				background: #E0E0E0;
-				color: black;
-			}
-			* {
-				box-sizing: border-box;
-			}
-		`;
-		document.head.appendChild(style);
-		//body.style.margin = "0";
-		//body.style.fontFamily = "-apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, Oxygen-Sans, Ubuntu, Cantarell, \"Helvetica Neue\", sans-serif";
-	}
-	render() {
-		return <DeviceSupport/>;
-	}
-};
+export default () => (
+	<React.Fragment>
+		<GlobalStyle/>
+		<DeviceSupport/>
+	</React.Fragment>
+);
