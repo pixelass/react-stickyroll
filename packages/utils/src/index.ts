cf52c514e7906c8d4cdfe57c124dcf95124fb403
Gregor Adams <greg@pixelass.com>
2018-12-08 02:14:51 +0100

    refactor: remove lodash and use own throttle

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./assert";
 export * from "./scroll-to";
+export * from "./throttle";
--- src/tracker/src/tracker.ts
+++ src/tracker/src/tracker.ts
@@ -1,4 +1,4 @@
-import throttle from "lodash.throttle";
+import {throttle} from "@stickyroll/utils";
 import {PureComponent} from "react";
 
 /**
