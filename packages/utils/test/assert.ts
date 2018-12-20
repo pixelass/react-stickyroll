6b73b9ad62e5dfc4f201a846fe8e02194e18a1b6
Gregor Adams <greg@pixelass.com>
2018-12-20 04:35:50 +0100

    test: adjust tests (coverage results)

--- packages/utils/test/assert.ts
+++ packages/utils/test/assert.ts
@@ -1,41 +1,5 @@
 import test from "ava";
-import {assert, classNames} from "../src";
-
-test("classNames joins classes", t => {
-	const expected = "1 2 3";
-	const actual = classNames(1, 2, 3);
-	t.is(expected, actual);
-});
-
-test("classNames ignores false", t => {
-	const expected = "1 2";
-	const actual = classNames(1, 2, null);
-	t.is(expected, actual);
-});
-
-test("classNames ignores null", t => {
-	const expected = "1 2";
-	const actual = classNames(1, 2, false);
-	t.is(expected, actual);
-});
-
-test("classNames ignores 0", t => {
-	const expected = "1 2";
-	const actual = classNames(0, 1, 2);
-	t.is(expected, actual);
-});
-
-test("classNames ignores empty strings", t => {
-	const expected = "1 2";
-	const actual = classNames(1, 2, "");
-	t.is(expected, actual);
-});
-
-test("classNames passes all options", t => {
-	const expected = "1 2 4 5 7 8 10 11";
-	const actual = classNames(0, 1, 2, null, 4, 5, false, 7, 8, "", 10, 11 );
-	t.is(expected, actual);
-});
+import {assert} from "../src";
 
 test("assert checks strings", t => {
 	t.throws(() => assert(1, "string"));
@@ -96,4 +60,5 @@ test("assert checks undefined", t => {
 	t.throws(() => assert({}, "undefined"));
 	t.notThrows(() => assert(undefined, "undefined"));
 });
+
 
---
+++ packages/utils/test/calls-names.ts
@@ -0,0 +1,38 @@
+import test from "ava";
+import {classNames} from "../src";
+
+test("classNames joins classes", t => {
+	const expected = "1 2 3";
+	const actual = classNames(1, 2, 3);
+	t.is(expected, actual);
+});
+
+test("classNames ignores false", t => {
+	const expected = "1 2";
+	const actual = classNames(1, 2, null);
+	t.is(expected, actual);
+});
+
+test("classNames ignores null", t => {
+	const expected = "1 2";
+	const actual = classNames(1, 2, false);
+	t.is(expected, actual);
+});
+
+test("classNames ignores 0", t => {
+	const expected = "1 2";
+	const actual = classNames(0, 1, 2);
+	t.is(expected, actual);
+});
+
+test("classNames ignores empty strings", t => {
+	const expected = "1 2";
+	const actual = classNames(1, 2, "");
+	t.is(expected, actual);
+});
+
+test("classNames passes all options", t => {
+	const expected = "1 2 4 5 7 8 10 11";
+	const actual = classNames(0, 1, 2, null, 4, 5, false, 7, 8, "", 10, 11 );
+	t.is(expected, actual);
+});
