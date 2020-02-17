d84d03929f48247a71ea547d7b86f3f54deb055d
Gregor Adams <greg@pixelass.com>
2020-02-17 14:50:59 +0100

    chore: update dependencies

--- src/testing-utils/src/jsdom.ts
+++ src/testing-utils/src/jsdom.ts
@@ -1,10 +1,10 @@
-import {JSDOM} from "jsdom";
+import {JSDOM, DOMWindow} from "jsdom";
 
 declare global {
 	namespace NodeJS {
 		interface Global {
 			document: Document;
-			window: Window;
+			window: DOMWindow;
 			navigator: Navigator | {userAgent: string};
 			requestAnimationFrame: (callback: any) => number;
 			cancelAnimationFrame: (id: any) => void;
